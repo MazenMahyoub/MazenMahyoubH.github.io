db5459e104435259b482b3da01eac1e382eb759f
MazenMahyoub <94761657+MazenMahyoub@users.noreply.github.com>
2022-01-27 20:44:20 +0300

    Add files via upload

---
+++ lab 2/src/Revers.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Revers {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4};
+        array = revers(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int[] revers(int[] array){
+        int [] arr2 = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arr2[array.length-i-1] = array[i];
+        }
+        return arr2;
+    }
+
+}
