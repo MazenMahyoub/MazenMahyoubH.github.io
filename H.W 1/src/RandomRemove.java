4f9007118b62845727289eb2c9d7166c5fea21fe
MazenMahyoub <94761657+MazenMahyoub@users.noreply.github.com>
2022-03-02 22:52:16 +0300

    Add files via upload

---
+++ H.W 1/src/RandomRemove.java
@@ -0,0 +1,18 @@
+package DataStructur.HomeWork.LabHomework;
+import java.util.*;
+
+public class RandomRemove {
+    static void removerandomly(int[] array) {
+        Random r = new Random();
+        while (array.length > 0) {
+            int index = r.nextInt(array.length);
+            System.out.println("INDEX = " + index + ", ELEMENT = " + array[index]);
+            int[] array1 = new int[array.length - 1];
+            for (int i = 0; i < index; i++)
+                array1[i] = array[i];
+            for (int i = index; i < array.length - 1; i++)
+                array1[i] = array[i + 1];
+            array = array1;
+        }
+    }
+}
